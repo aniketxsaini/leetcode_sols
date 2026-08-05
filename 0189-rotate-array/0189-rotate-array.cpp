013256f0e66ebea07a695e887815d889d3017779
Aniket <aniketxsaini@gmail.com>
2026-08-05 22:26:22 +0530

    Time: 0 ms (100%), Space: 29.6 MB (46.93%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k=k%nums.size();
+        if(nums.size()>k){
+            reverse(nums.end()-k,nums.end());
+        reverse(nums.begin(),nums.end()-k);
+        reverse(nums.begin(),nums.end());
+        }
+        
+    }
+};
