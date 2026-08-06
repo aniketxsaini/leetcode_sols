5ddcef20e61733ca894ba7cf92ab097e2c77a095
Aniket <aniketxsaini@gmail.com>
2026-08-06 17:39:54 +0530

    Time: 0 ms (100%), Space: 23.9 MB (56.69%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n=nums.size();
-        int low=0,high=0;
-        while(true){
-           while(low<n && nums[low]!=0){
-            low++;
-           }
-           if(low==n){
-            break;
-           }
-           high=low+1;
-           while(high<n && nums[high]==0){
-            high++;
-           }
-           if(high==n){
-            break;
-           }
-           swap(nums[low],nums[high]);
+        int j=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                swap(nums[i],nums[j]);
+                j++;
+            }
         }
+        
     }  
 };
