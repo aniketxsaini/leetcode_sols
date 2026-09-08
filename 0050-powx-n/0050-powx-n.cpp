3a82fbc4f7c63086b500b1392c3ca17d2e7b0a28
Aniket <aniketxsaini@gmail.com>
2026-09-08 21:36:55 +0530

    Time: 0 ms (100%), Space: 8.5 MB (71.89%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long N = n;
+        if(n<0){
+            x=1/x;
+            N=-N;
+        }
+        return power(x,N);
+    }
+    private:
+    double power(double x,long long N){
+        if(N==0){return 1;}
+        
+        double half = power(x,N/2);
+
+        if(N%2==0){
+            return half * half;
+        }
+
+        return x * half * half;
+    }
+    
+};
