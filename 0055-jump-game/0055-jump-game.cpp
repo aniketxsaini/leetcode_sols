af88400602c1d867eb143d9f54e59c21d1954f13
Aniket <aniketxsaini@gmail.com>
2026-09-19 21:57:09 +0530

    Time: 0 ms (100%), Space: 52.4 MB (57.57%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxIdx=0;
+        for(int i=0;i<nums.size();i++){
+            
+            if(i>maxIdx){
+                return false;
+            }
+            if(maxIdx>=nums.size()-1){
+                return true;
+            }
+            maxIdx=max(maxIdx,i+nums[i]);
+        }
+        return false;
+    }
+};
